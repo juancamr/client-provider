f4f7807a67e0df1067ab1be691d80dac0e78ec92
Juan Carlos <jcmrojas29@gmail.com>
2023-06-27 01:19:26 -0500

    on connect message mongodb

--- server/src/config/databaseConfig.ts
+++ server/src/config/databaseConfig.ts
@@ -1,5 +1,8 @@
-import mongoose from "mongoose";
+import mongoose, { MongooseError } from "mongoose";
 
 export async function connectToDatabase() {
-  await mongoose.connect(process.env.DB_URI ?? "");
+  await mongoose
+    .connect(process.env.DB_URI ?? "")
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error: MongooseError) => console.log(error))
 }
