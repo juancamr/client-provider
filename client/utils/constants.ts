2ef62576120711e97ed55b8b28a50e30b51b8bb2
Juan Carlos <jcmrojas29@gmail.com>
2023-07-01 03:31:05 -0500

    api constants

--- client/utils/constants.ts
+++ client/utils/constants.ts
@@ -1,1 +1,13 @@
-export const BASE_URL = "http://192.168.1.150:3000"
+export const BASE_URL = "http://192.168.1.150:3000";
+
+export const apis = {
+  user: {
+    REGISTER: "/api/public/user/register",
+    LOGIN: "/api/public/user/login",
+    IS_USERNAME_EXIST: "/api/public/user/is_username_exist",
+  },
+  email_code: {
+    GENERATE: "/api/verification_code/generate",
+    VERIFY: "/api/verification_code/verify",
+  },
+};
